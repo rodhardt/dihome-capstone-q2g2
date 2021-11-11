d1ca6fcb1475682bb05b344146f9b71dabc16ea0
fernandofrantz <fernandofrantz12@gmail.com>
2021-11-11 11:03:18 -0300

    feature: function callback added to the component

--- src/components/PayCard/index.tsx
+++ src/components/PayCard/index.tsx
@@ -6,6 +6,7 @@ interface PayCardProps {
   planName: string;
   anouncesQuantity: string;
   price: number;
+  callback?: (param?: any) => void;
 }
 
 export const PayCard = ({
@@ -13,6 +14,7 @@ export const PayCard = ({
   planName,
   anouncesQuantity,
   price,
+  callback,
 }: PayCardProps) => {
   return (
     <PayCardStyled>
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -4,16 +4,7 @@ import { HomepageStyled } from "./styles";
 function Homepage() {
   return (
     <>
-      <HomepageStyled>
-        Homepage
-        <PayCard
-          img="https://s01.jetimgs.com/trvAWQHuYcArjvEQrh93oEZSAxK0Jz8p2OIdynM1xiqde5wAy6ZXoYGmXXuKeq0D608YD4xIADjHZMyTvhQ4bsnjjkNUXE3veAI0piwVWZd2PoswD6DUDB3Fkb0/PSX_20190604_170651.jpg"
-          planName="Plano básico."
-          anouncesQuantity="Apenas 1 imóvel"
-          price={39.9}
-        />
-      </HomepageStyled>
-      ;
+      <HomepageStyled>Homepage</HomepageStyled>
     </>
   );
 }
