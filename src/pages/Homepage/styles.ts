8a0bff8f168a0722150d2d5c0cdf0c2965d89f83
fernandofrantz <fernandofrantz12@gmail.com>
2021-11-11 19:41:33 -0300

    update

--- src/pages/Homepage/styles.ts
+++ src/pages/Homepage/styles.ts
@@ -2,13 +2,8 @@ import styled from "styled-components";
 
 export const HomepageStyled = styled.main`
   display: flex;
-<<<<<<< HEAD
-  flex-direction: column;
-  justify-content: center;
-=======
   gap: 10px;
   justify-content: center;
   min-height: 100vh;
->>>>>>> 9f61be512082c9dec8c87de27c5e04ecff9a4aaf
   align-items: center;
 `;
