d3b892d15982a19d44f5c1bd4b33569c1bbe6264
gelosde <85689579+gelosde@users.noreply.github.com>
2021-11-12 09:30:23 -0300

    feat: header add

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,242 @@
 import styled from "styled-components";
 
-export const HeaderStyled = styled.header``;
+export const HeaderStyled = styled.div`
+  background-color: #0f9194;
+  @media only screen and (max-width: 320px) {
+    .notLoged {
+      display: flex;
+      .Link {
+        margin-top: 5%;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 4%;
+        margin-right: 2%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 13vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 5vh;
+        width: 100%;
+      }
+    }
+
+    .Logged {
+      display: flex;
+      .Link {
+        margin-top: 5%;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 4%;
+        margin-right: 2%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 13vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 5vh;
+        width: 100%;
+      }
+    }
+  }
+  @media only screen and (min-width: 321px) and (max-width: 375px) {
+    .notLoged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 5%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 5vh;
+        width: 100%;
+      }
+    }
+
+    .Logged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 5%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 5vh;
+        width: 100%;
+      }
+    }
+  }
+  @media only screen and (min-width: 376px) and (max-width: 425px) {
+    .notLoged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 5%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 6vh;
+        width: 100%;
+      }
+    }
+
+    .Logged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 7%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 6vh;
+        width: 100%;
+      }
+    }
+  }
+  @media only screen and (min-width: 426px) and (max-width: 767px) {
+    .notLoged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 5%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 6vh;
+        width: 100%;
+      }
+    }
+
+    .Logged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 7%;
+        font-size: 3vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 6vh;
+        width: 100%;
+      }
+    }
+  }
+  @media only screen and (min-width: 768px) and (max-width: 1023px) {
+    .notLoged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 7%;
+        font-size: 5vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 7vh;
+        width: 100%;
+      }
+    }
+
+    .Logged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 7%;
+        font-size: 5vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 7vh;
+        width: 100%;
+      }
+    }
+  }
+
+  @media only screen and (min-width: 1024px) {
+    .notLoged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 7%;
+        font-size: 5vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 7vh;
+        width: 100%;
+      }
+    }
+
+    .Logged {
+      display: flex;
+      .Link {
+        margin-top: 8vh;
+        text-decoration: none;
+        color: #ffffff;
+        margin-left: 7%;
+        font-size: 5vh;
+      }
+      .LinkImage {
+        height: 20vh;
+        width: 100%;
+      }
+      .LinkFont {
+        height: 7vh;
+        width: 100%;
+      }
+    }
+  }
+`;
