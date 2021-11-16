1e319c8abd146cb21c386a1ee58fcd95db1a5a4e
gelosde <85689579+gelosde@users.noreply.github.com>
2021-11-16 08:56:00 -0300

    feat: slide Show added

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,12 @@
 import { useState, useEffect } from "react";
+import CasaAsombrada from "../../assets/Images/CasaAssombrada.jpeg";
+import casaIt from "../../assets/Images/casaIt.jpeg";
+import gondor from "../../assets/Images/gondor.jpeg";
+import hobbt from "../../assets/Images/hobbt.jpg";
+import mansãoSpenser from "../../assets/Images/mansãoSpenser.jpeg";
+import evocandoEspiritos from "../../assets/Images/evocandoEspiritos.jpeg";
+import oExorcista from "../../assets/Images/oExorcista.jpeg";
+import alcatraz from "../../assets/Images/alcatraz.jpeg";
 
 import PropertyCard from "../../components/PropertyCard";
 import { useProperties } from "../../providers/Properties";
@@ -23,18 +31,6 @@ interface FilterSearchData {
 }
 
 function Dashboard() {
-  const [filterType, setFilTertype] = useState("");
-
-  const [filterBathroom, setFilterBathroom] = useState(0);
-
-  const [filterDorms, setFilterDorms] = useState(0);
-
-  const [filterPrice, setFilterPrice] = useState(0);
-
-  const [list, setList] = useState<PropertyData[]>([]);
-
-  const [searchInit, setSearchInit] = useState(false);
-
   const { properties } = useProperties();
 
   const [filteredProperties, setFilteredProperties] =
@@ -154,12 +150,96 @@ function Dashboard() {
     handleFilteredProperties();
   }, [activeFilters]);
 
+  const [conter, setConter] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      if (conter <= listFun.length - 2) setConter(conter + 1);
+      else setConter(0);
+    }, 5000);
+  }, [conter]);
+  const listFun = [
+    {
+      img: CasaAsombrada,
+      description:
+        "Bela casa para alugar em amityville aluguel acessível por apenas R$: 1.000 trincas e cadeados ja incluidos alem de ajuda na mudança",
+      lastResidentComent: "o preço é de matar",
+    },
+    {
+      img: casaIt,
+      description:
+        "Casa antiga a venda em Derry, Maine preço  R$:100.000 caso tenha crianças damos um desconto especial de 66.6% ",
+      lastResidentComent:
+        "A vizinhança é tão acolhedoura que voce nem tem que se preocupar com o futuro dos seus filhos",
+    },
+    {
+      img: gondor,
+      description:
+        "Belo castelo medieval a venda por R$: 1.000.000.000 exercito não incluido",
+      lastResidentComent: "O castelo é otimo mas o  vizinho é insuportavel",
+    },
+    {
+      img: hobbt,
+      description:
+        "Chale a preço amigavel nas montanhas R$: 80.000 com possiveis artefatos antigos",
+      lastResidentComent:
+        "Tirando ladrões de talheres os vizinhos são otimos......meu precioso....",
+    },
+    {
+      img: mansãoSpenser,
+      description:
+        "Antiga mansão ao stilo vitoriano a venda com um porão muito grande R$: 1.000.000 caso tenha a cura para o T-virus a um desconto de 30%",
+      lastResidentComent:
+        "Os vizinhos parecem meio mortos mas se tiver muita munição é bem calmo....",
+    },
+    {
+      img: evocandoEspiritos,
+      description:
+        "Casa de 2 andares com fazenda proxima para alugar R$: 1.300 o mes ",
+      lastResidentComent:
+        "A um cheiro estranho vindo das paredes pode precisar de detetizador",
+    },
+    {
+      img: oExorcista,
+      description: "",
+      lastResidentComent: "",
+    },
+    {
+      img: alcatraz,
+      description:
+        "Ilha paradiziaca com complexo habtacional semi-novo incluido",
+      lastResidentComent: "Uma vez dentro nunca mais vai querer( e poder) sair",
+    },
+  ];
   return (
     <>
+      <div>
+        <button
+          onClick={() => {
+            if (conter > 0) setConter(conter - 1);
+          }}
+        >
+          before
+        </button>
+        <section>
+          <img src={listFun[conter].img} alt="imagem casa" />
+          <p>{listFun[conter].description}</p>
+          <p>{listFun[conter].lastResidentComent}</p>
+        </section>
+
+        <button
+          onClick={() => {
+            if (conter <= listFun.length - 2) setConter(conter + 1);
+            else setConter(0);
+          }}
+        >
+          next
+        </button>
+      </div>
+
       {isChoosingFilters && (
         <FilterModal handleFilter={handleFilter} closeWindow={closeWindow} />
       )}
-      <button onClick={() => handleFilteredProperties()}>Teste</button>
+      {/* <button onClick={() => handleFilteredProperties()}>Teste</button> */}
       <button onClick={() => setIsChoosingFilters(true)}>Abrir Filtros</button>
       <DashboardStyled>
         {properties &&
