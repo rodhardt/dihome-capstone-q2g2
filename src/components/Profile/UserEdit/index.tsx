ebef2c49f166b1c2ab2cb9deaa0fab8509042f2b
gelosde <85689579+gelosde@users.noreply.github.com>
2021-11-17 15:55:26 -0300

    feat: added test

---
+++ src/components/Profile/UserEdit/index.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { UserEditStyled } from "./styles";
+
+import { useAuth } from "../../../providers/Authentication";
+
+interface UserEditProps {
+  closeModal: () => void;
+}
+
+function UserEdit({ closeModal }: UserEditProps) {
+  const { userInfo, updateUser } = useAuth();
+
+  const [inputName, setInputName] = useState(userInfo.name);
+  const [inputEmail, setInputEmail] = useState(userInfo.email);
+  const [inputTelephone, setInputTelephone] = useState(userInfo.telephone);
+
+  const handleInputName = () => {
+    if (inputName === userInfo.name) {
+      setInputName("");
+    }
+  };
+  const handleInputEmail = () => {
+    if (inputEmail === userInfo.email) {
+      setInputEmail("");
+    }
+  };
+  const handleInputTelephone = () => {
+    if (inputTelephone === userInfo.telephone) {
+      setInputTelephone("");
+    }
+  };
+
+  const submitChanges = () => {
+    const newUserInfo = {
+      id: userInfo.id,
+      name: inputName,
+      email: inputEmail,
+      telephone: inputTelephone,
+      consultant: userInfo.consultant,
+      announcedProperties: userInfo.announcedProperties,
+      bookmarkedProperties: userInfo.bookmarkedProperties,
+      subscriptionType: userInfo.subscriptionType,
+      markedDates: userInfo.markedDates,
+    };
+    updateUser(newUserInfo);
+  };
+
+  return (
+    <UserEditStyled>
+      <div className="modal-card">
+        <header>
+          <h4>Alterar Informações</h4>
+
+          <button className="close-button" onClick={() => closeModal()}>
+            X
+          </button>
+        </header>
+        <div className="form-container">
+          <div className="input-container">
+            <label htmlFor="input-name">Nome</label>
+            <input
+              id="input-name"
+              value={inputName}
+              onClick={() => handleInputName()}
+              onChange={(evt) => setInputName(evt.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="input-email">E-mail</label>
+            <input
+              id="input-email"
+              value={inputEmail}
+              onClick={() => handleInputEmail()}
+              onChange={(evt) => setInputEmail(evt.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="input-telephone">Telefone</label>
+            <input
+              id="input-telephone"
+              value={inputTelephone}
+              onClick={() => handleInputTelephone()}
+              onChange={(evt) => setInputTelephone(evt.target.value)}
+            />
+          </div>
+        </div>
+        <div className="buttons-container">
+          <button
+            className="confirm-button"
+            onClick={() => {
+              submitChanges();
+              closeModal();
+            }}
+          >
+            Alterar
+          </button>
+          <button
+            className="cancel-button"
+            onClick={() => {
+              closeModal();
+            }}
+          >
+            cancelar
+          </button>
+        </div>
+      </div>
+    </UserEditStyled>
+  );
+}
+
+export default UserEdit;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,11 +8,13 @@ export default createGlobalStyle`
         outline: 0;
         font-size: 16px;
         font-family: 'Space Mono', monospace;
+        
     }
 
     :root {
+        --lightGray:#606060;
         --mainGray: #F1F1F1;
-        --darkOrange: ##E08043;
+        --darkOrange: #E08043;
         --orange: #F08847;
         --lightOrange: #FFA166;
         --darkBlue: #0F9194;
@@ -20,9 +22,36 @@ export default createGlobalStyle`
         --opacityBlue: #0F919425;
         --opacityOrange: #F0884790;
         --mainFont: 'Space Mono', monospace;
+        --darkGray: #404040;;
     }
 
     button:hover {
         cursor: pointer;
+    }
+
+    body {
+        background-color: var(--mainGray);
+        min-height: 100vh;
+        position: absolute;
+        max-width: 100vw;
+        width: 100%;
+        height: 100%;
+    }
+
+    main, section {
+        background-color: var(--mainGray);
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    a, a:hover, a:focus, a:active {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    main {
+        margin-top: 50px;
     }
 `;
